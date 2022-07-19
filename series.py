71d8269e20952cb66bc4f82d0f9e47a2546b29d4
Vinny <vinnyshipley@gmail.com>
2022-07-19 14:14:49 -0700

    initial commit with work through the first 2 test sets done

---
+++ series.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+
+    if n == 1 or n == 2:
+        return 1
+
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+def lucas(n):
+    if n == 0:
+        return 2
+
+    if n == 1:
+        return 1
+
+    else:
+        return lucas(n-1) + lucas(n-2)
